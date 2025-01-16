7b2f56709352b53c5386995d2278c3ff913d5078
IOQRST <dmiitryl95@gmail.com>
2025-01-16 23:10:51 +0300

    add python installation by default

--- archinstall/UEFI/UEFI_pt3.sh
+++ archinstall/UEFI/UEFI_pt3.sh
@@ -9,4 +9,4 @@ EDITOR=nano visudo
 nano /etc/pacman.conf
 
 pacman -Syyu --noconfirm
-pacman -S tar zip unzip p7zip unrar xz gzip nodejs npm ninja gcc cmake meson go rust lib32-mesa lib32-vulkan-radeon lib32-mesa-vdpau pipewire pipewire-alsa pipewire-jack pipewire-pulse gst-plugin-pipewire wireplumber openssh freerdp networkmanager-openconnect networkmanager-openvpn networkmanager-vpnc networkmanager-l2tp networkmanager-pptp network-manager-applet network-manager-sstp --noconfirm
+pacman -S tar zip unzip p7zip unrar xz gzip python3 python-pip nodejs npm ninja gcc cmake meson go rust lib32-mesa lib32-vulkan-radeon lib32-mesa-vdpau pipewire pipewire-alsa pipewire-jack pipewire-pulse gst-plugin-pipewire wireplumber openssh freerdp networkmanager-openconnect networkmanager-openvpn networkmanager-vpnc networkmanager-l2tp networkmanager-pptp network-manager-applet network-manager-sstp --noconfirm
